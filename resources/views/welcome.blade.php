f70644a2811bbd0e7f040220cf59d7ad2afb09cc
fowr <64537320+fowr@users.noreply.github.com>
2021-06-08 17:58:56 +0430

    landing page view

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel App</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+            *{
+                font-family: 'B yekan';
+            }
+            body{
+               background: black;
+                height: 100vh;
+                display: grid;
+                justify-content: center;
+                align-content: center
+            }
+            a{
+                line-height: 150%;
+                text-decoration: underline;
+                color: rgb(71, 126, 230);
+               
+            }
+            h1{
+                color: white;
+            }
+            h2{
+                text-align: center
+            }
+        </style>
+     
+    </head>
+    <body class="antialiased bc-black flex items-top justify-center items-center " style="height:100vh" >
+        <div>
+          
+            <h2>
+                <a href="/todos" > ورود به برنامه</a>
+            </h2>
+
+        </div>
+    </body>
+</html>
